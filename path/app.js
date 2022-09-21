d983595702ac17a843c5b0aaba4c48d504887b5c
Md Arif <106609511+arif013@users.noreply.github.com>
2022-09-21 21:50:26 +0530

    Pagination corrected

--- path/app.js
+++ path/app.js
@@ -1,32 +1,37 @@
-console.log("Here we go");
+console.log("Let's start the game")
 
 let pokebox = document.getElementById("pokebox");
-const fetchPokemon=()=>{
-    const promises=[];
-    for(let i=1;i<=200;i++){
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}?`;
-        promises.push(fetch(url).then((response) => response.json()));
-    }
-    Promise.all(promises).then((result) => {
-        const pokemon=result.map((data)=>({
-            name: data.name,
-            weight: data.weight,
-            height:data.height,
-            image: data.sprites.front_default,
-            type: data.types.map((type)=>type.type.name).join('')
-        }))
-        displayPokemon(pokemon);
-    })
+var pokemon;
+const promises =[];
+
+// Data is fetched from URL 
+for(let i=1;i<51;i++){
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`
+    promises.push(fetch(url).then((response) => response.json()));
 }
+
+// Data is mapped in required format
+Promise.all(promises).then(result =>{
+     pokemon = result.map(data=>({
+        name: data.name,
+        weight: data.weight,
+        height:data.height,
+        image: data.sprites.front_default,
+        type: data.types.map((type)=>type.type.name).join('')
+    }))
+    displayPokemon(pokemon);
+})
+
+// Buttons and number of elements shown
 let prev = document.getElementById("button_prev");
 let next = document.getElementById("button_next");
 let start = 0;
 let pageSize = 12;
-const displayPokemon=(pokemon)=>{
-    pokebox.innerHTML='';
-    console.log(pokemon);
+
+// Pokemons displayed
+const displayPokemon = (pokemon)=>{
+        pokebox.innerHTML='';
     let item = pokemon.slice (start, start+pageSize);
-    console.log(item);
     const PokemonHtml =item.map((poke)=> `
         <li>
         <h2 class="heading"><strong>${poke.name.toUpperCase()}</strong></h2>
@@ -36,19 +41,27 @@ const displayPokemon=(pokemon)=>{
         <p>Type: ${poke.type}</p>
         </li>`).join('');
         pokebox.innerHTML+=PokemonHtml;
-        prev.addEventListener('click', ()=>{
-            if(start>=pageSize){
-                start = start - pageSize;
-                displayPokemon(pokemon);
-            }
-        })
-        next.addEventListener('click', ()=>{
-            if(start+pageSize<pokemon.length){
-                start = start + pageSize;
-                displayPokemon(pokemon);
-            }
-        })
-        
     }
-fetchPokemon();
+    
+// Previous button function
+prev.addEventListener('click', ()=>{
+    if(start>=pageSize){
+        start = start - pageSize;
+        displayPokemon(pokemon);
+        console.log(start)
+    }else{
+        button.disabled = true
+    }
+})
+
+// Next button function
+next.addEventListener('click', ()=>{
+    if(start+pageSize<pokemon.length){
+        start = start + pageSize;
+        displayPokemon(pokemon);
+        console.log(start)
+    }
+})
+    
+
 
